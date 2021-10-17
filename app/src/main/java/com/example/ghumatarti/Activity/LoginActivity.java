47c71dbb78273807614837e257687635bcee723c
lynessadsouza <lynessadsouza@gmail.com>
2021-10-17 23:15:48 +0530

    User Login

---
+++ app/src/main/java/com/example/ghumatarti/Activity/LoginActivity.java
@@ -0,0 +1,105 @@
+package com.example.ghumatarti.Activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.ghumatarti.ModelClass.UserModel;
+import com.example.ghumatarti.R;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextInputEditText emaill, passwordl;
+    MaterialButton login;
+
+    DatabaseReference getusers;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        initUi();
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getusers = FirebaseDatabase.getInstance().getReference("Users");
+                //     Query query = getusers.child("Users").orderByChild("email").equalTo(e);
+                Log.d("before query", "bfore query");
+                Log.d("before query", "" + emaill.getText().toString().trim());
+
+                //   Query q=
+
+                Query query = getusers.orderByChild("email").equalTo(emaill.getText().toString().trim());
+
+                Log.d("query", "" + query);
+//query starts here
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Log.d("datasnapshoot", "" + dataSnapshot);
+
+                        if (dataSnapshot.exists()) {
+                            // dataSnapshot is the "issue" node with all children with id 0
+
+                            for (DataSnapshot user : dataSnapshot.getChildren()) {
+                                // do something with the individual "issues"
+
+                                Log.d("user", "" + user);
+
+
+                                UserModel userModel = user.getValue(UserModel.class);
+                                Log.d("user model password", "" + userModel.getPassword());
+                                Log.d("email.gdt text", "" + emaill.getText().toString().trim());
+
+                                if (userModel.getPassword().equals(passwordl.getText().toString().trim())) {
+                                    Log.d("user model getpass", "" + userModel.getPassword());
+                                    Log.d("user model password", "" + userModel.getEmail());
+                                    Log.d("user model password", "" + userModel.getName());
+
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(intent);
+                                } else {
+                                    Toast.makeText(getApplicationContext(), "Password is wrong", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        } else {
+                            Toast.makeText(getApplicationContext(), "User not found", Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+//ends here database reference
+            }
+        });
+
+
+    }
+
+    //Init UI
+    private void initUi() {
+        emaill = (TextInputEditText) findViewById(R.id.email_login);
+        passwordl = (TextInputEditText) findViewById(R.id.password_login);
+        login = (MaterialButton) findViewById(R.id.loginbtn);
+    }
+    //Init UI ends here
+}
--- app/src/main/java/com/example/ghumatarti/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/ghumatarti/Activity/RegisterActivity.java
@@ -8,14 +8,20 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.ghumatarti.ModelClass.UserModel;
 import com.example.ghumatarti.R;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textview.MaterialTextView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
 
     MaterialButton register;
-    TextInputEditText name, phone, email, password, cnf_password;
+    TextInputEditText name1, phone1, email1, password1, cnf_password;
+
+    MaterialTextView signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,49 +31,63 @@ public class RegisterActivity extends AppCompatActivity {
 
         initUI();
 
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-               validateFields();
+                validateFields();
 
 
-                Log.d("validated","Validated");
+                Log.d("validated", "Validated");
 
 
             }
 
             private void validateFields() {
 
-                if (name.getText().toString().isEmpty()) {
+                if (name1.getText().toString().isEmpty()) {
                     Toast.makeText(getApplicationContext(), "Please Enter name", Toast.LENGTH_LONG).show();
                     //   name.requestFocus();
-                    name.setFocusable(true);
-                } else if (phone.getText().toString().isEmpty()) {
-                    Toast.makeText(getApplicationContext(), "Please Enter name", Toast.LENGTH_LONG).show();
+                    name1.requestFocus();
+                } else if( (phone1.getText().toString().isEmpty()) ||( phone1.getText().toString().length()<10)||( phone1.getText().toString().length()>10) ){
+                        Toast.makeText(getApplicationContext(), "Please Enter Phone Number currectly", Toast.LENGTH_LONG).show();
                     //   name.requestFocus();
-                    phone.setFocusable(true);
-                } else if (email.getText().toString().isEmpty()) {
-                    Toast.makeText(getApplicationContext(), "Please Enter name", Toast.LENGTH_LONG).show();
+                    phone1.requestFocus();
+                } else if (email1.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Please Enter email", Toast.LENGTH_LONG).show();
                     //   name.requestFocus();
-                    email.setFocusable(true);
-                } else if (password.getText().toString().isEmpty()) {
-                    Toast.makeText(getApplicationContext(), "Please Enter name", Toast.LENGTH_LONG).show();
+                    email1.requestFocus();
+
+                } else if (password1.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Please Enter password", Toast.LENGTH_LONG).show();
                     //   name.requestFocus();
-                    password.setFocusable(true);
+                    password1.requestFocus();
                 } else if (cnf_password.getText().toString().isEmpty()) {
                     Toast.makeText(getApplicationContext(), "Please Enter name", Toast.LENGTH_LONG).show();
                     //   name.requestFocus();
-                    cnf_password.setFocusable(true);
-                } else if (password.getText().toString().equals(cnf_password.getText().toString()) != true) {
+                    cnf_password.requestFocus();
+                } else if (password1.getText().toString().equals(cnf_password.getText().toString()) != true) {
                     Toast.makeText(getApplicationContext(), "PASSWORD AND CONFIRM PASSWORD NOT MATCHING", Toast.LENGTH_LONG).show();
-                    cnf_password.setFocusable(true);
-                }
-                else
-                {
+                    cnf_password.requestFocus();
+                } else {
                     Toast.makeText(getApplicationContext(), "Validated", Toast.LENGTH_LONG).show();
-                    Intent i= new Intent(RegisterActivity.this,MainActivity.class);
+
+                    //Save Users To database here
+                    
+                    StoreUsers();
+
+
+
+                    Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
                     startActivity(i);
 
                 }
@@ -76,15 +96,58 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
+    private void StoreUsers() {
+        String name;
+        String phone;
+        String email;
+        String password;
+
+        name=name1.getText().toString().trim();
+         phone=phone1.getText().toString().trim();
+         email=email1.getText().toString().trim();
+         password=password1.getText().toString().trim();
+
+
+
+        FirebaseDatabase rootNode = FirebaseDatabase.getInstance();
+        DatabaseReference reference = rootNode.getReference("Users");
+
+       // encodeEmail(email);
+        //encode--> cant add . to the database root node
+String id= email.replace(".",",");
+
+
+//to decode
+  /*      String id= email.replace(",",".");*/
+
+
+
+        UserModel addNewUser = new UserModel(name, phone , email, password);
+        reference.child(id).setValue(addNewUser);
+
+        //We will also create a Session here in next videos to keep the user logged In
+
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+        finish();
+
+    }
+
+ /*   private String encodeEmail(String email) {
+        return email.replace(".", ",");
+
+
+    }
+*/
     private void initUI() {
 
         register = (MaterialButton) findViewById(R.id.register);
 
-        name = (TextInputEditText) findViewById(R.id.name);
-        phone = (TextInputEditText) findViewById(R.id.phone);
-        email = (TextInputEditText) findViewById(R.id.email);
-        password = (TextInputEditText) findViewById(R.id.password);
+        name1 = (TextInputEditText) findViewById(R.id.name);
+        phone1 = (TextInputEditText) findViewById(R.id.phone);
+        email1 = (TextInputEditText) findViewById(R.id.email);
+        password1 = (TextInputEditText) findViewById(R.id.password);
         cnf_password = (TextInputEditText) findViewById(R.id.cnf_password);
+        signin = (MaterialTextView) findViewById(R.id.redirect_to_login);
 
     }
 }
